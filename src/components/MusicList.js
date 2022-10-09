b37e4565ed0bba692befbde99223ce08cccf69d6
dv-jamie <sjm2506@gmail.com>
2022-10-10 02:29:36 +0900

    MusicList 추가

---
+++ src/components/MusicList.js
@@ -0,0 +1,27 @@
+import musicList from "../databases/musicList"
+import "./MusicList.css"
+
+function MusicList() {
+    return (
+        <ul className="music-list-container">
+            {musicList.map(music => {
+                return (
+                    <li className="music-list">
+                        <div className="album-cover-wrap">
+                            <img src={music.image} />
+                        </div>
+                        <div className="music-info">
+                            <p className="info-title">{music.title}</p>
+                            <p className="info-artist">{music.artist}</p>
+                        </div>
+                        <span className="add-icon material-symbols-outlined">
+                            playlist_add
+                        </span>
+                    </li> 
+                )
+            })}
+        </ul>
+    )
+}
+
+export default MusicList
---
+++ src/databases/musicList.js
@@ -0,0 +1,74 @@
+const musicList = [
+    {
+        "title": "LOVE DIVE",
+        "artist": "IVE",
+        "image": "https://image.bugsm.co.kr/album/images/500/40737/4073710.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "FEARLESS",
+        "artist": "LE SSERAFIM (르세라핌)",
+        "image": "https://image.bugsm.co.kr/album/images/500/40751/4075173.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "Advice",
+        "artist": "태민 (TAEMIN)",
+        "image": "http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/082/050/244/82050244_1621244234115_1_600x600.JPG",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "Stay This Way",
+        "artist": "프로미스나인",
+        "image": "https://image.bugsm.co.kr/album/images/500/40771/4077170.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "Pink Venom",
+        "artist": "BLACKPINK",
+        "image": "https://blackpinkofficial.com/music/born-pink/release-bornpink/img/ds.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "DICE",
+        "artist": "NMIXX",
+        "image": "https://i.scdn.co/image/ab67616d0000b273c8caa659d37a00d34cbd6359",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "FOREVER 1",
+        "artist": "소녀시대 (GIRLS' GENERATION)",
+        "image": "https://image.bugsm.co.kr/album/images/200/204845/20484595.jpg?version=20220818012053.0",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "SNEAKERS",
+        "artist": "ITZY (있지)",
+        "image": "https://i.scdn.co/image/ab67616d0000b273e61bca92e4a64e50ee44a009",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "도깨비불 (Illusion)",
+        "artist": "aespa",
+        "image": "https://image.bugsm.co.kr/album/images/200/204715/20471520.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    },
+    {
+        "title": "Attention",
+        "artist": "NewJeans",
+        "image": "https://blog.kakaocdn.net/dn/bFwRtb/btrIJValNCk/yy6K60RiHRqXzaNxTjVEMK/img.jpg",
+        "audio": "",
+        "lyrics": "가사.."
+    }
+]
+
+export default musicList
