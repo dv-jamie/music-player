2db127fd29e035513015e28dd169df125692c5fb
dv-jamie <sjm2506@gmail.com>
2022-10-10 03:46:42 +0900

    PlayArea 추가

---
+++ src/components/PlayArea.js
@@ -0,0 +1,68 @@
+import "./PlayArea.css"
+
+function PlayArea() {
+    return (
+        <div className="play-area-container">
+            <ul className="tabs">
+                <li>재생 화면</li>
+                <li>재생 목록</li>
+            </ul>
+            <div className="album-cover-wrap">
+                <img src="https://image.bugsm.co.kr/album/images/500/40737/4073710.jpg" />
+            </div>
+            <div className="played-music-info">
+                <p className="title">제목</p>
+                <p className="artist">가수</p>
+            </div>
+            <div className="lyrics">
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+                가사..<br />
+            </div>
+        </div>
+    )
+}
+
+export default PlayArea
