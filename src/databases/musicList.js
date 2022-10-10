b937c7980a52acfd435cd8dc1e7e1cc5c6027ea1
dv-jamie <sjm2506@gmail.com>
2022-10-10 12:55:14 +0900

    musicList - id 추가

--- src/databases/musicList.js
+++ src/databases/musicList.js
@@ -1,5 +1,6 @@
 const musicList = [
     {
+        "id": 1,
         "title": "LOVE DIVE",
         "artist": "IVE",
         "image": "https://image.bugsm.co.kr/album/images/500/40737/4073710.jpg",
@@ -7,6 +8,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 2,
         "title": "FEARLESS",
         "artist": "LE SSERAFIM (르세라핌)",
         "image": "https://image.bugsm.co.kr/album/images/500/40751/4075173.jpg",
@@ -14,6 +16,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 3,
         "title": "Advice",
         "artist": "태민 (TAEMIN)",
         "image": "http://image.genie.co.kr/Y/IMAGE/IMG_ALBUM/082/050/244/82050244_1621244234115_1_600x600.JPG",
@@ -21,6 +24,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 4,
         "title": "Stay This Way",
         "artist": "프로미스나인",
         "image": "https://image.bugsm.co.kr/album/images/500/40771/4077170.jpg",
@@ -28,6 +32,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 5,
         "title": "Pink Venom",
         "artist": "BLACKPINK",
         "image": "https://blackpinkofficial.com/music/born-pink/release-bornpink/img/ds.jpg",
@@ -35,6 +40,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 6,
         "title": "DICE",
         "artist": "NMIXX",
         "image": "https://i.scdn.co/image/ab67616d0000b273c8caa659d37a00d34cbd6359",
@@ -42,6 +48,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 7,
         "title": "FOREVER 1",
         "artist": "소녀시대 (GIRLS' GENERATION)",
         "image": "https://image.bugsm.co.kr/album/images/200/204845/20484595.jpg?version=20220818012053.0",
@@ -49,6 +56,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 8,
         "title": "SNEAKERS",
         "artist": "ITZY (있지)",
         "image": "https://i.scdn.co/image/ab67616d0000b273e61bca92e4a64e50ee44a009",
@@ -56,6 +64,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 9,
         "title": "도깨비불 (Illusion)",
         "artist": "aespa",
         "image": "https://image.bugsm.co.kr/album/images/200/204715/20471520.jpg",
@@ -63,6 +72,7 @@ const musicList = [
         "lyrics": "가사.."
     },
     {
+        "id": 10,
         "title": "Attention",
         "artist": "NewJeans",
         "image": "https://blog.kakaocdn.net/dn/bFwRtb/btrIJValNCk/yy6K60RiHRqXzaNxTjVEMK/img.jpg",
